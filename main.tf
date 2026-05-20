53313018facea3264cd5a2285ff4765a90598572
France <francevanmaanen@gmail.com>
2026-05-20 11:09:54 +0200

    Fix Terraform formatting

--- main.tf
+++ main.tf
@@ -7,39 +7,39 @@ terraform {
     }
   }
 }
- 
+
 provider "aws" {
   region = var.aws_region
 }
- 
+
 resource "aws_s3_bucket" "app_assets" {
   bucket = "${var.project_name}-${var.environment}-assets-francevanmaanen"
- 
+
   tags = {
     Name        = "${var.project_name}-assets"
     Environment = var.environment
     ManagedBy   = "Terraform"
   }
 }
- 
+
 resource "aws_s3_bucket_versioning" "app_assets" {
   bucket = aws_s3_bucket.app_assets.id
- 
+
   versioning_configuration {
     status = "Enabled"
   }
 }
- 
+
 resource "aws_dynamodb_table" "app_state" {
   name         = "${var.project_name}-${var.environment}-state"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
- 
+
   attribute {
     name = "LockID"
     type = "S"
   }
- 
+
   tags = {
     Name        = "${var.project_name}-state"
     Environment = var.environment
